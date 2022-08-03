4ccb21a8b2117ed8e5d9c8b9f6b8c498cce88c25
Alvaro Diaz <alvarodiazjr@yahoo.com>
2022-08-03 03:13:33 +0000

    when character is clicked their full info is shown, and home button returns to home page.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
+// var $form = document.querySelector('form');
 var $charList = document.querySelector('.append');
 var $homePage = document.querySelector('.home-page');
 var $homeButton = document.querySelector('.home-button');
+var $fullCharInfo = document.querySelector('.full-char-info');
 
 var xhr = new XMLHttpRequest();
 
@@ -8,12 +10,16 @@ xhr.open('GET', 'https://www.breakingbadapi.com/api/characters?category=Breaking
 
 xhr.responseType = 'json';
 
+// --- This Function renders the pictures to the home page.------
+
 xhr.addEventListener('load', function () {
   for (var i = 0; i < xhr.response.length; i++) {
     if (xhr.response[i].nickname === 'Holly') {
       continue;
     }
     var characters = xhr.response[i];
+    data.characters.push(characters);
+    data.charId.push(characters.char_id);
 
     var $column = document.createElement('div');
     $column.setAttribute('class', 'column-one-fifth column-half');
@@ -25,6 +31,7 @@ xhr.addEventListener('load', function () {
     var $img = document.createElement('img');
     $img.setAttribute('src', characters.img);
     $img.setAttribute('alt', 'character-img');
+    $img.setAttribute('id', characters.name);
     $imgBox.appendChild($img);
 
     $charList.appendChild($column);
@@ -32,9 +39,70 @@ xhr.addEventListener('load', function () {
   return $charList;
 });
 
-xhr.send();
+// ------------------------------------------------------
 
 $charList.addEventListener('click', function () {
+  for (var i = 0; i < xhr.response.length; i++) {
+    if (xhr.response[i].nickname === 'Holly') {
+      continue;
+    }
+    if (xhr.response[i].name === event.target.id) {
+
+      var characters = xhr.response[i];
+
+      var $row = document.createElement('div');
+      $row.setAttribute('class', 'row nowrap info-card');
+
+      var $cardImg = document.createElement('div');
+      $cardImg.setAttribute('class', 'card-img');
+      $row.appendChild($cardImg);
+
+      var $img = document.createElement('img');
+      $img.setAttribute('src', characters.img);
+      $img.setAttribute('alt', 'character-img');
+      $cardImg.appendChild($img);
+
+      var $cardText = document.createElement('div');
+      $cardText.setAttribute('class', 'column-full card-text');
+      $row.appendChild($cardText);
+
+      var $name = document.createElement('h1');
+      $name.textContent = characters.name;
+      $cardText.appendChild($name);
+
+      var $nickName = document.createElement('h3');
+      $nickName.textContent = 'Nickname: ' + characters.nickname;
+      $cardText.appendChild($nickName);
+
+      var $birthday = document.createElement('h3');
+      $birthday.textContent = 'DOB: ' + characters.birthday;
+      $cardText.appendChild($birthday);
+
+      var $occupation = document.createElement('h3');
+      $occupation.textContent = 'Occupation: ' + characters.occupation;
+      $cardText.appendChild($occupation);
+
+      var $status = document.createElement('h3');
+      $status.textContent = 'Status: ' + characters.status;
+      $cardText.appendChild($status);
+
+      var $actor = document.createElement('h3');
+      $actor.textContent = 'Actor: ' + characters.portrayed;
+      $cardText.appendChild($actor);
+
+      $fullCharInfo.prepend($row);
+    }
+  }
   $homePage.classList.add('hidden');
   $homeButton.classList.remove('hidden');
+  $fullCharInfo.classList.remove('hidden');
+  return $fullCharInfo;
 });
+
+$homeButton.addEventListener('click', function () {
+  $homePage.classList.remove('hidden');
+  $homeButton.classList.add('hidden');
+  $fullCharInfo.classList.add('hidden');
+});
+
+xhr.send();
